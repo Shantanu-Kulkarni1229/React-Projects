3b944dfb7fcefdc50e81799f4de1f4923fd1ebdb
Shantanu-Kulkarni1229 <shantanukulkarni1229@gmail.com>
2025-04-15 00:01:55 +0530

    feat: initialize React Router project with Tailwind CSS and Vite
    Added initial project setup including React Router, Tailwind CSS, and Vite configuration. Created basic components and routes for Home, About, Contact, and User pages. Configured Tailwind CSS and PostCSS for styling. Added Vite and React Router dependencies.

---
+++ 07reactrouter/src/main.jsx
@@ -0,0 +1,49 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {createRoutesFromChildren, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import Layout from './Layout.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import { createBrowserRouter, Route } from'react-router-dom'
+import { User } from './components/user/User.jsx'
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} >
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      {/* <Route path="github" element={<Github />} /> */}
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
